e2b7b0fb42d4760cde2fce6a807b565721797826
miguelangelsp7161@gmail.com <miguelangelsp7161@gmail.com>
2023-08-16 19:33:01 -0400

    Ejercicio Compañero kotlin

---
+++ Ejercicios ING. HELEN/com.kt
@@ -0,0 +1,22 @@
+
+ fun main() {
+ var comp1:String = "Carlos"
+ var comp1Edad:Int = 22
+ println("Me llamo "+comp1+" y tengo "+ comp1Edad)
+    
+ var comp2:String = "Pedro"
+ var comp2Edad:Int = 24
+ println("Me llamo "+comp2+" y tengo "+ comp2Edad)
+        
+  var comp3:String = "Mario"
+  var comp3Edad:Int = 26
+  println("Me llamo "+comp3+" y tengo "+ comp3Edad)
+      
+  var comp4:String = "Eduardo"
+  var comp4Edad:Int = 24
+  println("Me llamo "+comp4+" y tengo "+ comp4Edad)
+      
+  var comp5:String = "Héctor"
+  var comp5Edad:Int = 22
+  println("Me llamo "+comp5+" y tengo "+ comp5Edad)
+}
