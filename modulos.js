1614521c873e1af9ad210d8e728c5926377df805
Merksand <miguelangelsp7161@gmail.com>
2023-10-10 17:45:32 -0400

    Modulo

---
+++ modulos.js
@@ -0,0 +1,13 @@
+console.clear();
+const personajes = ()=>{
+    let per =["Maria","Luis","Mario"]
+    console.log(per);
+}
+personajes()
+let frutas = ["Manzana", "Pera", "Sandia"]
+console.log(frutas);
+(()=>{
+    'use strict'
+    let colores = ["Rojo","Amarillo","Verde"]
+    console.log(colores);
+})()
